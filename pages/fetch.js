c57e96c8fe687b2cf65a061dca573f37a8314a85
Davidveliz91 <davidveliz.qac@gmail.com>
2024-06-16 23:08:12 -0300

    add:search function

--- pages/fetch.js
+++ pages/fetch.js
@@ -1,38 +1,60 @@
 const $personajesContainer = document.getElementById("personajes")
 
-fetch("https://hp-api.onrender.com/api/characters").
-  then((respuesta) => {
-    // console.log(respuesta)
-    return respuesta.json();
-  })
-  .then((data) => {
-    const personajes = data;
-    fetchPersonajes = data
-    //console.log(personajes)
-    personajes.forEach((personaje) => {
-      let srcImage;
-      if(personaje.image) {
-        srcImage = personaje.image
-      } else {
-        srcImage = "./assetspersonajes/1081-producto-71wd9qtqvll-ac-ul1500-3330.jpg"
-      }
-      
-      $personajesContainer.innerHTML += `
-      <div>   
-          <img src="${srcImage}" alt="imagen de ${personaje.name}">
-          <h4>${personaje.name}</h4>
-          <p>Genero: ${personaje.gender}</p>
-          <p>Fecha de Nacimiento:${personaje.yearOfBirth}</p>
-          <p>Raza:${personaje.ancestry}</p>
-          <p>Casa:${personaje.house}</p>
-          <p>Patronus:${personaje.patronus}</p>
-      </div>
-    `;
-    });
 
-  })
-
-  function search() {
-    console.log("Buscando!")
+function createCardPersonaje(personaje) {
+  let srcImage;
+  if (personaje.image) {
+    srcImage = personaje.image
+  } else {
+    srcImage = "./assetspersonajes/1081-producto-71wd9qtqvll-ac-ul1500-3330.jpg"
   }
+
+  $personajesContainer.innerHTML += `
+  <div>   
+      <img src="${srcImage}" alt="imagen de ${personaje.name}">
+      <h4>${personaje.name}</h4>
+      <p>Genero: ${personaje.gender}</p>
+      <p>Fecha de Nacimiento:${personaje.yearOfBirth}</p>
+      <p>Raza:${personaje.ancestry}</p>
+      <p>Casa:${personaje.house}</p>
+      <p>Patronus:${personaje.patronus}</p>
+  </div>
+`;
+}
+
+function includeName(personaje, name) {
+  const str = personaje.name.toLowerCase()
+  if (str.includes(name)) {
+    return personaje
+  }
+}
+
+function fetching(name) {
+  fetch("https://hp-api.onrender.com/api/characters").
+    then((respuesta) => {
+      return respuesta.json();
+    })
+    .then((data) => {
+      let personajes;
+      if (name) {
+        $personajesContainer.innerHTML = ""
+        personajes = data.filter((personaje) => includeName(personaje, name))
+      } else {
+        personajes = data;
+      }
+
+      personajes.forEach(createCardPersonaje);
+    })
+}
+
+
+function search() {
+  console.log("Buscando!")
+  const searchInput = document.getElementById('buscador');
+  const searched = searchInput.value.toLowerCase();
+  fetching(searched)
+
+}
+
+fetching()
 
