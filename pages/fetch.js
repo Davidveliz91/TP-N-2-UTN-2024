eed61a41c163870311edd5496de789a1d14f3ad6
Davidveliz91 <davidveliz.qac@gmail.com>
2024-06-16 21:25:59 -0300

    add:update function js

--- pages/fetch.js
+++ pages/fetch.js
@@ -1,4 +1,4 @@
-const $main = document.querySelector("main")
+const $personajesContainer = document.getElementById("personajes")
 
 fetch("https://hp-api.onrender.com/api/characters").
   then((respuesta) => {
@@ -9,18 +9,23 @@ fetch("https://hp-api.onrender.com/api/characters").
     const personajes = data;
     //console.log(personajes)
     personajes.forEach((personaje) => {
-      $main.innerHTML += `
-<div>   
- 
-    <img src="${personaje.image}" alt="imagen de ${personaje.name}">
-    <h4>${personaje.name}</h4>
-    <p>Genero: ${personaje.gender}</p>
-    <p>Fecha de Nacimiento:${personaje.yearOfBirth}</p>
-    <p>Raza:${personaje.ancestry}</p>
-    <p>Casa:${personaje.house}</p>
-    <p>Patronus:${personaje.patronus}</p>
-</div>
+      let srcImage;
+      if(personaje.image) {
+        srcImage = personaje.image
+      } else {
+        srcImage = "./assetspersonajes/1081-producto-71wd9qtqvll-ac-ul1500-3330.jpg"
+      }
 
+      $personajesContainer.innerHTML += `
+      <div>   
+          <img src="${srcImage}" alt="imagen de ${personaje.name}">
+          <h4>${personaje.name}</h4>
+          <p>Genero: ${personaje.gender}</p>
+          <p>Fecha de Nacimiento:${personaje.yearOfBirth}</p>
+          <p>Raza:${personaje.ancestry}</p>
+          <p>Casa:${personaje.house}</p>
+          <p>Patronus:${personaje.patronus}</p>
+      </div>
     `;
     });
 
